e85293fbd031214965cf1ec42edfaa3b8a9bf33c
Pascal-Juma <pascaljuma2002@gmail.com>
2025-05-05 21:20:04 +0300

    Build the entier project

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,63 @@
-import { useState } from "react"
-import Axios from "axios"
+import { useState } from "react";
+import Axios from "axios";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage } from "@cloudinary/react";
+import { fill } from "@cloudinary/url-gen/actions/resize";
+import "./App.css";
 
 function App() {
-  const [imageSeleted, setImageSelected] = useState("");
+  const [imageSelected, setImageSelected] = useState<File | null>(null);
+  const [publicId, setPublicId] = useState<string | null>(null);
   const uploadImage = () => {
     const formData = new FormData();
-    formData = new formData();
-    formData.append("file", imageSelected);
-    formData.append("upload_preset", "");
+    if (imageSelected) {
+      formData.append("file", imageSelected);
+    }
+    formData.append("upload_preset", "ajp9cgop");
 
-    Axios.post("https://api.cloudinary.com/v1_1//image/upload",
+    Axios.post(
+      "https://api.cloudinary.com/v1_1/do89liaoq/image/upload",
       formData
     ).then((response) => {
-      console.log(response);
+      setPublicId(response.data.public_id);
     });
+  };
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: "do89liaoq",
+    },
+  });
+
+  let myImage = null;
+  if (publicId) {
+    myImage = cld.image(publicId);
+    myImage.resize(fill().width(300).height(300));
   }
   return (
-    <div>
-      <input
-      type="file"
-      onChange={(event) =>{
-        setImageSelected(event.target.files[0]);
-      }}
-      />
-      <button type="button" onClick={uploadImage}>Upload Image</button>
-    </div>
-  )
+    <>
+      <div className="section">
+        <label htmlFor="fileInput">Upload File:</label>
+        <input
+          id="fileInput"
+          type="file"
+          title="Choose a file to upload"
+          onChange={(e) => {
+            if (e.target.files && e.target.files[0]) {
+              setImageSelected(e.target.files[0]);
+            }
+          }}
+        />
+        <button type="button" onClick={uploadImage}>
+          Upload Image
+        </button>
+      </div>
+      {myImage && (
+        <div className="image">
+          <AdvancedImage cldImg={myImage} />
+        </div>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
